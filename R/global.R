9adb89e4856e1dbaf204ad626f2f461ac0d6a8ef
younessbahi <ybahi.email@gmail.com>
2023-02-17 00:18:35 +0100

    minor changes

--- R/global.R
+++ R/global.R
@@ -9,8 +9,8 @@ globalVariables(
 "display_text_range", "entities", "expanded_url", "ext", "ext_edit_control", "extended_entities", "geo",
   "globalObjects", "id_str", "indices", "inline", "media", "name", "original_info", "result_context",
   "rounded_score", "rowID", "screen_name", "sizes", "social_context", "timeline", "tokens", "topic",
-  "tweet_volume", "tweets", "urls", "user_id_str", "user_mentions", "users", "value",
+  "tweet_volume", "tweets", "urls", "user_id_str", "user_mentions", "users", "value", "listed_count",
     "favorite_count", "favourites_count", "followers_count", "friends_count", "row_id",
-    "media_count", "normal_followers_count", "quote_count", "reply_count",
+    "media_count", "normal_followers_count", "quote_count", "reply_count", "fast_followers_count",
     "retweet_count", "statuses_count", "r", "info", "aspect_ratio", "variants", "video_info")
 )
